50488bd128f7bdebfd5ae4ce345e641795218ac2
Roberty Yin <robertyin7@gmail.com>
2022-05-08 10:46:51 +0000

    first commit

---
+++ assets/js/webconnect.js
@@ -0,0 +1,22 @@
+const { Web3 } = require("./web3")
+
+const getWeb3 = async () => {
+    return new Promise(async (resolve, reject) => {
+        const web3 = new Web3(window.ethereum)
+
+        try{
+            await window.ethereum.request({ method: "eth_requestAccounts" })
+            resolve(web3)
+        } catch(error) {
+            reject(error)
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("connect_button", addEventListener, "click", async() => {
+        const web3 = await getWeb3()
+        // const walletAddress = web3.eth.requestAccounts()
+        // const walletBalanceInWei = await 
+    })
+})
